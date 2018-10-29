783e63e5b60838ba5bab34837fcc664136085bf5
Thierry Silvagnoli <thierry.martins.r.s@gmail.com>
2018-10-29 13:35:50 -0300

    needs more style and optional

--- app/controllers/questions_controller_controller.rb
+++ app/controllers/questions_controller_controller.rb
@@ -1,3 +1,15 @@
 class QuestionsControllerController < ApplicationController
+  def answer
 
+    unless params[:question].nil?
+      @question = params[:question].downcase
+      if @question == "I am going to work".downcase
+        @answer =  "Great"
+      elsif @question.last == "?"
+        @answer = "Silly question, get dressed and go to work!."
+      else
+        @answer = "I don't care, get dressed and go to work!"
+      end
+    end
+  end
 end
