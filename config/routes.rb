4b18c43fc81ad990e318dfaa6c107a9d9c941b29
Thierry Silvagnoli <thierry.martins.r.s@gmail.com>
2018-10-29 13:36:23 -0300

    route changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "pages/about"
   get "pages/contact"
+  get 'questions_controller/answer'
 end
